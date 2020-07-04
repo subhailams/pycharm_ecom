b0ffb226a01214bca8f9494a25d0e3ff472fb940
unknown <sanjaysan648@gmail.com>
2020-07-04 17:56:47 +0530

    changes in production

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -116,6 +116,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
 AUTHENTICATION_BACKENDS = [
     
     'django.contrib.auth.backends.ModelBackend',
