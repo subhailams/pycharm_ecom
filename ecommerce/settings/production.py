55fee0dac7c6d5ba92e2ff90956ce9a78738b7a6
unknown <sanjaysan648@gmail.com>
2020-07-05 01:29:20 +0530

    reverting whitenoise

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -63,7 +63,7 @@ AUTH_USER_MODEL = 'accounts.User'
 LOGIN_URL='/applogin/'
 LOGIN_URL_REDIRECT='/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MIDDLEWARE = [
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
