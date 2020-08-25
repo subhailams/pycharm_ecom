38e3438e9a053fed148a6fde21c8d82d61b60c20
unknown <sanjaysan648@gmail.com>
2020-08-25 19:54:33 +0530

    changes

--- cart/views.py
+++ cart/views.py
@@ -34,9 +34,11 @@ def cart_detail_api_view(request):
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    if cart_obj.isordered== True:
-        request.session['cart_items']==0
+    if cart_obj.isordered:
+        print("*****")
+        request.session['cart_items'] = 0
         del request.session['cart_id']
+		
     return render(request, "carts/cart.html", {"cart": cart_obj})
 
 def cart_update(request):
