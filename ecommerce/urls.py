26989b7b599ed232f9b5cafcdf39071c8e6843b2
unknown <sanjaysan648@gmail.com>
2020-10-25 22:23:37 +0530

    protocols

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -49,28 +49,9 @@ urlpatterns = [
     url(r'^orders/', include(("orders.urls",'orders'), namespace='orders')),
     # url('temp', TemplateView.as_view(template_name="socialapp/login1.html")),
     url(r'accounts/', include('allauth.urls')),
-    url(r'^password/change/$', 
-                auth_views.PasswordChangeView.as_view(), 
-                name='password_change'),
-    url(r'^password/change/done/$',
-            auth_views.PasswordChangeDoneView.as_view(), 
-            name='password_change_done'),
-    url(r'^password/reset/$', 
-            auth_views.PasswordResetView.as_view(template_name = "registeration/password_reset_form.html"), 
-            name='password_reset'),
-    url(r'^password/reset/done/$', 
-            auth_views.PasswordResetDoneView.as_view(template_name = "registeration/password_reset_done.html"), 
-            name='password_reset_done'),
-    url(r'^password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 
-            auth_views.PasswordResetConfirmView.as_view(template_name = "registeration/password_reset_confirm.html"), 
-            name='password_reset_confirm'),
-    url(r'^password/reset/complete/$', 
-            auth_views.PasswordResetCompleteView.as_view(template_name = "registeration/password_reset_complete.html"), 
-            name='password_reset_complete'),
-
     url(r'^accounts/$', RedirectView.as_view(url='/account')),
     url(r'^account/', include(("accounts.urls",'account'), namespace='account')),
-
+    url(r'^accounts/', include("accounts.passwords.urls")),
 
 ]
 
